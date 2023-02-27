52b36cecfea12fc7b646a818b4cfc2a42b4ea5f4
viraj sankalpa <viraj@pop-os.localdomain>
2023-02-27 20:06:02 +0530

    get method

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+// console.log("viraj sankalpa");
+
+app.get('/',(req,res)=>{
+    res.send("Hello world");
+
+})
+
+
+app.listen(8080,()=>{
+    console.log("Server is Working");
+});
