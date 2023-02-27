9940d3551faebadd7f66afed1da47463751eeb9b
viraj sankalpa <viraj@pop-os.localdomain>
2023-02-27 14:42:42 +0530

    getting wanted details is ok

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const getWeather = require('./utils/weather');
+const getGeo = require('./utils/getLocation');
 
-getWeather(13,-94)
+
+getGeo('kandy')
+    .then((location)=>{
+        // console.log(location);
+        return getWeather(location.lat,location.lon);
+    })
     .then((data)=>{
         console.log(data);
     })
-    .catch((err)=>{
-        console.log(err);
+    .catch((error)=>{
+        console.log(error);
     })
+
+// getWeather(13,-94)
+//     .then((data)=>{
+//         console.log(data);
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+//     })
--- utils/getLocation.js
+++ utils/getLocation.js
@@ -1,17 +1,31 @@
 const axios = require('axios');
 
+const getGeo = (city)=>{
+    return new Promise((resolve,reject)=>{
+        const locationUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=1&appid=e11cfdcd19951791ca0c0943ac7529c3';
+
+        axios.get(locationUrl)
+        .then(function (response) {
+            resolve({location:response.data[0].name,
+                     lat: response.data[0].lat ,
+                     lon: response.data[0].lon,
+                     })
+            // handle success
+            // console.log(response.data[0].name + " is in lat = " + response.data[0].lat +"and lon = " + response.data[0].lon + " ==> " + response.data[0].country);
+
+        })
+        .catch(function (error) {
+            reject({err : "Unable to find the location"})
+            // handle error
+            // console.log("Unable to find the location");
+        });
+
+
+            })
+}
 
 //get location
-const city = "matale";
-const locationUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=1&appid=e11cfdcd19951791ca0c0943ac7529c3';
+// const city = "matale";
 
-axios.get(locationUrl)
-  .then(function (response) {
-    // handle success
-    console.log(response.data[0].name + " is in lat = " + response.data[0].lat +"and lon = " + response.data[0].lon + " ==> " + response.data[0].country);
 
-  })
-  .catch(function (error) {
-    // handle error
-    console.log("Unable to find the location");
-  });
+module.exports = getGeo;
